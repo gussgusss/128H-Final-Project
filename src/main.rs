006d303d009f18074806c25ed058948038d224ff
gusektarit <ektarit.n@cmkl.ac.th>
2025-04-10 00:37:51 -0500

    Implemented an echo server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,33 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    // echo server
+    let listener = TcpListener::bind("localhost:4040").await.unwrap();
+
+    // ensures multiple clients can connect
+    loop {
+        let (mut socket, address) = listener.accept().await.unwrap();
+
+        // spawns an async thread for each client
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            // for more than 1 message
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                // loop breaks when client disconnects
+                if (bytes_read == 0) {
+                    break;
+                }
+                writer.write_all(line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
+    }
 }
