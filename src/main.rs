c76c923d494d6bc5a8425a5d41db8d1233868db9
gusektarit <ektarit.n@cmkl.ac.th>
2025-04-10 23:17:40 -0500

    Chatting between users

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     // echo server
     let listener = TcpListener::bind("localhost:4040").await.unwrap();
+    let (tx, _) = broadcast::channel(10);
 
-    // ensures multiple clients can connect
+    // ensures multiple users can connect
     loop {
         let (mut socket, address) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
-        // spawns an async thread for each client
+        // spawns an async thread for each user
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
             let mut reader = BufReader::new(reader);
@@ -20,13 +24,26 @@ async fn main() {
 
             // for more than 1 message
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                // loop breaks when client disconnects
-                if (bytes_read == 0) {
-                    break;
+                // returns first branch that finishes - user sends msg / receiving msg from other user
+                tokio::select! {
+                    user_msg = reader.read_line(&mut line) => {
+                        // loop breaks when user disconnects
+                        if (user_msg.unwrap() == 0) {
+                            break;
+                        }
+
+                        tx.send((line.clone(), address)).unwrap();
+                        line.clear();
+                    }
+                    other_msg = rx.recv() => {
+                        let (mut msg, mut other_address) = other_msg.unwrap();
+
+                        // doesn't show the message if it has the user's address
+                        if (other_address != address) {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
                 }
-                writer.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
             }
         });
     }
